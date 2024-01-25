760951e51eb9794757fca603057f5587268c6a84
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2024-01-25 11:25:39 -0300

    feat: add script commands to restar server and init sql

--- arkhanna.sh
+++ arkhanna.sh
@@ -6,4 +6,17 @@ server_up(){
 	sudo docker compose logs -f
 }
 
+server_restart(){
+	set -e
+	sudo docker compose down
+	server_up
+}
+
+init_db(){
+	set -e
+	files=$(ls rathena/sql-files/*.sql)
+	files+=$(ls rathena/sql-files/upgrades/*.sql)
+	# TODO exec sql script files on remote server
+}
+
 eval $@
